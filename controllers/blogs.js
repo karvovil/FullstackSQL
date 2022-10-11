2fdfcaea752de661240ed7bdafab9311928700d2
Ville Karvonen <villekarvonen1@gmail.com>
2022-10-11 15:27:26 +0300

    13.7 Added error handling

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,38 +3,57 @@ const router = require('express').Router()
 const { Blog } = require('../models')
 
 router.get('/', async (req, res) => {
-    const blogs = await Blog.findAll()
-  
-    const blogStrings = blogs.map(b => 
-      `${b.id} ${b.author}: '${b.title}', ${b.likes} likes`
-    )
-    blogStrings.map(bs => console.log(bs))
-    res.json(blogStrings)
-  })
+  const blogs = await Blog.findAll()
+
+  const blogStrings = blogs.map(b => 
+    `${b.id} ${b.author}: '${b.title}', ${b.likes} likes`
+  )
+  blogStrings.map(bs => console.log(bs))
+  res.json(blogStrings)
+})
   
   //router.use(express.json())
   
-  router.post('/', async (req, res) => {
-    try{
-      console.log(req.body)
-      const blog = await Blog.create(req.body)
-      return res.json(blog)
-    } catch(error) {
-      return res.status(400).json({ error })
-    }
-  })
-  router.delete('/:id', async (req, res) => {
-    await Blog.destroy({ where: { id: req.params.id } })
-    res.status(204).end()
-  })
-  router.put('/:id', async (req, res) => {
+router.post('/', async (req, res, next) => {
+  try{
+    console.log(req.body)
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    next(error)
+  }
+})
+router.delete('/:id', async (req, res) => {
+  await Blog.destroy({ where: { id: req.params.id } })
+  res.status(204).end()
+})
+router.put('/:id', async (req, res, next) => {
+  try {
     const blog = await Blog.findByPk(req.params.id)
     if (blog) {
-        blog.likes = req.body.likes
-        await blog.save()
-        res.json(blog)
-      } else {
-        res.status(404).end()
-      }
-  })
-  module.exports = router
+      blog.likes = req.body.likes
+      await blog.save()
+      res.json(blog)
+    } else {
+      throw { name:"ShitId", message: "try better id" }
+    } 
+  } catch (error) {
+      next(error)        
+  }
+})
+const errorHandler = (error, request, response, next) => {
+  console.error(error.name)
+  console.error(error.message)
+
+  if (error.name === 'ShitId') {
+    return response.status(404).send({ error: 'no such id' })
+  }else if (error.name === 'SequelizeValidationError') {
+    return response.status(400).send({ error: 'malformatted blog' })
+  }else if (error.name === 'SequelizeDatabaseError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+router.use(errorHandler)
+
+module.exports = router
