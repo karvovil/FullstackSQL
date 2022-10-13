482dfc55c56f807cc5f0f54531ecc4eb033e4515
Ville Karvonen <villekarvonen1@gmail.com>
2022-10-13 12:51:44 +0300

    13.11 only authorized users can add blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,46 @@
 const router = require('express').Router()
+const { Blog, User } = require('../models')
 
-const { Blog } = require('../models')
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      console.log(authorization.substring(7))
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error){
+      console.log(error)
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
-
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userId'] },
+    include: {
+      model: User,
+      attributes: ['name']
+    }
+  })
+  console.log(blogs.map(b=>b.user))
   const blogStrings = blogs.map(b => 
-    `${b.id} ${b.author}: '${b.title}', ${b.likes} likes`
+    `id: ${b.id} ${b.user ? 'user: '+b.user.name :''} author: ${b.author} titel: '${b.title}', ${b.likes} likes`
   )
   blogStrings.map(bs => console.log(bs))
   res.json(blogStrings)
 })
   
-  //router.use(express.json())
-  
-router.post('/', async (req, res, next) => {
+router.post('/', tokenExtractor, async (req, res, next) => {
   try{
     console.log(req.body)
-    const blog = await Blog.create(req.body)
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({...req.body, userId: user.id, date: new Date()})
     return res.json(blog)
   } catch(error) {
     next(error)
