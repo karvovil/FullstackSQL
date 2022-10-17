f89ff11289e24cf8cf62287745924bdff54584d4
Ville Karvonen <villekarvonen1@gmail.com>
2022-10-17 11:57:54 +0300

    13.22 blogs in readinglists can be marked read with PUT to api/readinglists/id

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 const { Membership } = require('../models')
+const {tokenExtractor} = require('../util/middleware')
 
 router.post('/', async (req, res, next) => {
   try {
@@ -10,6 +11,23 @@ router.post('/', async (req, res, next) => {
         read: false
     })
     res.json(membership)
+  } catch(error) {
+    next(error)
+  }
+})
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  try {
+    const mmship = await Membership.findByPk(req.params.id)
+    if (!mmship){
+      throw { name: "Shit Membership Id", message: "try better memebership id" }
+    }
+    console.log(mmship, req.decodedToken)
+    if ( mmship.blogId !== req.decodedToken.id){
+      res.status(403).end()
+    }
+    mmship.read = req.body.read
+    await mmship.save()
+    res.json(mmship)
   } catch(error) {
     next(error)
   }
